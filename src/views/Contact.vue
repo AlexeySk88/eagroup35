f8893024d29896a050d727858d0d48c2685556e8
Alexey Skriplenok <skriplenok88@mai.ru>
2018-10-20 15:40:03 +0300

    #

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -57,8 +57,37 @@
 		            center: [c, f],
 		            zoom: 15
 		        });
-		        DG.marker([c, f]).addTo(map);
+		        DG.marker([c, f]).addTo(map); 
 		    });
+
+
+		    (function (d, w, c) {
+          (w[c] = w[c] || []).push(function() {
+              try {
+                  w.yaCounter50632792 = new Ya.Metrika2({
+                      id:50632792,
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true,
+                      webvisor:true
+                  });
+            var yaCounter50632792 = new Ya.Metrika({id: 50632792});
+		    yaCounter50632792.hit('/contact', {
+		        title: 'Контактная информация'});
+              } catch(e) { }
+          });
+
+          var n = d.getElementsByTagName("script")[0],
+              s = d.createElement("script"),
+              f = function () { n.parentNode.insertBefore(s, n); };
+          s.type = "text/javascript";
+          s.async = true;
+          s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+          if (w.opera == "[object Opera]") {
+              d.addEventListener("DOMContentLoaded", f, false);
+          } else { f(); }
+      })(document, window, "yandex_metrika_callbacks2");
 		}
 	}
  </script>
